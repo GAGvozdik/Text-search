528c390d9eed0889891e163571c419c0af8ede1c
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2021-01-06 13:53:36 +0300

    vo

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _10_textbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            
+            textBox2.Text = File.ReadAllText(@"text.txt");
+
+            StreamReader file = new StreamReader("text.txt");
+            string line;
+            int i = 0;
+            int c = 0;
+            while (!file.EndOfStream)
+            {
+                i += 1;
+                line = file.ReadLine();
+                if (textBox1.Text == line)
+                {
+                    MessageBox.Show(i.ToString());
+                    c = 1;
+                }
+                
+
+            }
+            if (c==0)
+            {
+                MessageBox.Show("не нашлось");
+            }
+
+
+
+
+        }
+    }
+}
